3abbad45e3deffcbd4bee6c5eb5597cdc8bdc167
mdaviot <mdaviot@3f623432-82b3-11de-9a75-9d4ddcaa5d2c>
2009-12-20 22:47:25 +0000

    obsolete

--- MagicMatchups/src/com/tyrcho/magic/matchups/client/service/EventServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.tyrcho.magic.matchups.client.service;
-
-import com.tyrcho.magic.matchups.client.model.Event;
-import com.tyrcho.magic.matchups.client.widget.sae.AsyncSAEService;
-
-public interface EventServiceAsync extends AsyncSAEService<Event>{
-}
